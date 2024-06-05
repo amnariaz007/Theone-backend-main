87448eaeb3cd0f7afbb0029d0a36cedeba9a2922
amnariaz007 <amanamana606@gmail.com>
2024-06-06 03:24:23 +0500

    commit

--- Models/FairLaunchModel.js
+++ Models/FairLaunchModel.js
@@ -8,55 +8,55 @@ const fairLaunchSchema = new mongoose.Schema(
     },
     currency: {
       type: String,
-   
+      required: true,
     },
     wallet: {
       type: String,
-   
+      required: true,
     },
     freeOption: {
       type: String,
-   
+      required: true,
     },
     totalSellAmount: {
       type: Number,
-   
+      required: true,
     },
     liquidityLockup: {
       type: Number,
-   
+      required: true,
     },
     routerType: {
       type: String,
-   
+      required: true,
     },
     softCap: {
       type: Number,
-   
+      required: true,
     },
     maxOwner: {
       type: Number,
-   
+      required: true,
     },
     liquidity: {
       type: Number,
-   
+      required: true,
     },
     startDate: {
       type: Date,
-   
+      required: true,
     },
     endDate: {
       type: Date,
-   
+      required: true,
     },
     logo: {
       type: String,
-   
+      required: true,
     },
     website: {
       type: String,
-   
+      required: true,
     },
     socialSites: {
       type: [],
@@ -64,11 +64,11 @@ const fairLaunchSchema = new mongoose.Schema(
     },
     tokenInfo: {
       type: Object,
-   
+      required: true,
     },
     description: {
       type: String,
-   
+      required: true,
     },
     status: {
       type: String,
